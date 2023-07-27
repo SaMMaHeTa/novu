ce16eb17691927705f08ce4fcd9d6deba4e2fb64
Sagar Maheta <50017070+SaMMaHeTa@users.noreply.github.com>
2023-07-27 19:27:05 +0530

    mongo config

--- docker helper.sh
+++ docker helper.sh
@@ -36,21 +36,25 @@ docker rm c4af0f56e261 50ae0da422c1 ea3c6a49f349 0602fb8e3edf 09d6b51da90c 82305
 
 
 
-c847645ee909 abdb7832e157 cb6c0f6aea0a 44feb2bba92f 2232ca1f1bcd a5fd53f0e226 1a3dafe19d30 c545fe4a7fe2
+fb5fba25b25a c1dc010e6f24 df2ef11054d7 ce111cd23bc9 efed20bb6d34 e885e92574f1 e131ac2f8d18
 
 
 
-e58421575dff fb5fba25b25a c1dc010e6f24 7e89539dd8bd df2ef11054d7 ce111cd23bc9 efed20bb6d34 e885e92574f1 e131ac2f8d18
 
-
-
-devcontainer_app                                latest    e58421575dff   16 hours ago    1.28GB
 mongo                                           latest    fb5fba25b25a   13 days ago     654MB
 redis                                           alpine    c1dc010e6f24   2 weeks ago     30.2MB
-redis                                           latest    7e89539dd8bd   2 weeks ago     130MB
 ghcr.io/novuhq/novu/worker                      0.16.0    df2ef11054d7   6 weeks ago     2.02GB
 ghcr.io/novuhq/novu/api                         0.16.0    ce111cd23bc9   6 weeks ago     2.02GB
 ghcr.io/novuhq/novu/widget                      0.16.0    efed20bb6d34   6 weeks ago     2.07GB
 ghcr.io/novuhq/novu/embed                       0.16.0    e885e92574f1   6 weeks ago     1.3GB
 ghcr.io/novuhq/novu/web                         0.16.0    e131ac2f8d18   6 weeks ago     183MB
-ghcr.io/novuhq/novu/ws 
+ghcr.io/novuhq/novu/ws      
+
+
+db.createUser(
+{
+"user": "TE@Admin",
+"pwd": "7r8h3e8Y$Q#A",
+"roles": [ { role: "userAdminAnyDatabase", db: "admin" } ]
+}
+)
